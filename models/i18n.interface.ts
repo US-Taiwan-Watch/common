c0972310582817b26095ccadaa43b2d2ee19546d
Robin Liao <yilia@microsoft.com>
2022-03-19 00:22:44 -0700

    i18n

---
+++ models/i18n.interface.ts
@@ -0,0 +1,44 @@
+import { ObjectType, Field, Info } from "type-graphql";
+
+export interface Ii18NText {
+  zh?: string;
+  en?: string;
+}
+
+/**
+ * I18NText
+ */
+@ObjectType()
+export class I18NText implements Ii18NText {
+  @Field(() => String, { nullable: true })
+  zh?: string;
+
+  @Field(() => String, { nullable: true })
+  en?: string;
+
+  // computed field
+  @Field((type) => String, { nullable: true })
+  text(@Info() info: { variableValues?: any }): string {
+    const lang: string = info.variableValues?.lang;
+    let s = this.zh || this.en || "";
+    if (lang) {
+      switch (lang.toLowerCase().substring(0, 2)) {
+        case "en":
+          s = this.en ?? s;
+          break;
+
+        case "zh":
+        default:
+          s = this.zh ?? s;
+      }
+    }
+    return s;
+  }
+
+  public static create(en?: string, zh?: string): I18NText {
+    const text = new I18NText();
+    zh && (text.en = en);
+    zh && (text.zh = zh);
+    return text;
+  }
+}
--- models/index.ts
+++ models/index.ts
@@ -1,2 +1,3 @@
+export * from "./i18n.interface";
 export * from "./models.interface";
 export * from "./user.interface";

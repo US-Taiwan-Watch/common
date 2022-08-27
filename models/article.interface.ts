36c6bc7dad77c1867914ed77451db969e2967ff0
Renee Hsu <reneehsu@Renees-MacBook-Pro.local>
2022-08-27 10:14:45 -0700

    add article fields

--- models/article.interface.ts
+++ models/article.interface.ts
@@ -8,15 +8,18 @@ export type ArticleStatus = 'Draft' | 'Publish';
  */
 @ObjectType()
 export class Article {
-    constructor(title?: string, content?: string, status?: ArticleStatus, author?: string[], imageSource?: string, tags?: string[]) {
+    constructor(title?: string, content?: string, slug?: string, preview?: string, isPublished?: boolean, authors?: string[], imageSource?: string, tags?: string[]) {
         this.id = uuidv4();
         this.title = title;
         this.content = content;
-        this.status = status;
-        this.author = author;
+        this.slug = slug;
+        this.preview = preview;
+        this.isPublished = isPublished;
+        this.authors = authors;
         this.imageSource = imageSource;
         this.tags = tags;
     }
+
 
     @Field(() => String, { nullable: false })
     id!: string;
@@ -28,13 +31,25 @@ export class Article {
     content?: string;
 
     @Field(() => String, { nullable: true })
-    status?: ArticleStatus;
-
-    @Field(() => [String], { nullable: true })
-    author?: string[];
+    slug?: string;
 
     @Field(() => String, { nullable: true })
-    pusblishTime?: string;
+    preview?: string;
+
+    @Field(() => Boolean, { nullable: true })
+    isPublished?: boolean;
+
+    @Field(() => [String], { nullable: true })
+    authors?: string[];
+
+    @Field(() => Number, { nullable: true })
+    pusblishTime?: number;
+
+    @Field(() => Number, { nullable: true })
+    createdTime?: number;
+
+    @Field(() => Number, { nullable: true })
+    lastModifiedTime?: number;
 
     @Field(() => String, { nullable: true })
     imageSource?: string;

f8f717bc1b3841a7c4e1257dc642592b8cb7bd7e
Ching-Wen Hsu <cwhsu@hq.bill.com>
2023-02-06 20:57:10 -0800

    tmp

--- models/index.ts
+++ models/index.ts
@@ -3,5 +3,7 @@ export * from "./models.interface";
 export * from "./user.interface";
 export * from "./member.interface";
 export * from "./bill.interface";
+export * from "./tag.interface";
+export * from "./notion-page.interface";
 export * from "./article.interface";
 export * from "./article.input";
---
+++ models/notion-page.interface.ts
@@ -0,0 +1,3 @@
+export class NotionPage {
+  notionPageId?: string;
+}

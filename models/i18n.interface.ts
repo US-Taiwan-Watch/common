e718187a3a579c0b09cfcee59512196ab013fc40
Ching-Wen Hsu <hsugaigai@gmail.com>
2023-06-10 19:29:40 -0700

    move i18n text field to resolver

--- models/i18n.interface.ts
+++ models/i18n.interface.ts
@@ -1,5 +1,4 @@
-import { ObjectType, Field, Info, InputType, Ctx } from "type-graphql";
-import { IApolloContext } from "../../src/@types/common.interface";
+import { ObjectType, Field, InputType } from "type-graphql";
 
 export interface Ii18NText {
   zh?: string;
@@ -25,32 +24,6 @@ export class I18NText implements Ii18NText {
 
   @Field(() => String, { nullable: true })
   en?: string;
-
-  // computed field
-  @Field(type => String, { nullable: true })
-  text(
-    @Info() info: { variableValues?: any },
-    @Ctx() ctx: IApolloContext,
-  ): string {
-    const lang: string = info.variableValues?.lang || ctx.language;
-    let s = this.zh || this.en || "";
-    if (lang) {
-      switch (lang.toLowerCase().substring(0, 2)) {
-        case "en":
-          s = this.en ?? s;
-          break;
-
-        case "zh":
-        default:
-          s = this.zh ?? s;
-      }
-    }
-    return s;
-  }
-
-  public static create(en?: string, zh?: string): I18NText {
-    return new I18NText({ zh, en });
-  }
 
   public constructor(txt?: Ii18NText) {
     this.zh = txt?.zh;
